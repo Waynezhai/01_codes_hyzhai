bac536e43cd81486482b544ead95fdd08d88421e
Waynezhai <root@localhost.localdomain>
2019-07-29 19:47:08 +0800

    增加clacE1的鲁棒性

--- 02_codes_Python/sc_005_calcE1.py
+++ 02_codes_Python/sc_005_calcE1.py
@@ -5,7 +5,7 @@
 # Author: Wayne_zhy
 # Mail: zhyzhaihuiyan@163.com
 # Created Time: 2019-7-29 18:42:12 
-# Last Modified: 2019-07-29 19:08:35
+# Last Modified: 2019-07-29 19:46:22
 ################################################################# 
 
 """
@@ -19,6 +19,9 @@
 import getopt
 import sys
 
+algorithm_a = ""
+channel_a = "000"
+alg = ""
 
 def usage():
     print 'Usage:\n' \
@@ -32,6 +35,31 @@ def usage():
 def version():
     print "Version: 1.0.1"
     sys.exit()
+    
+def calc(channel_a, algorithm_a):
+    a = int(channel_a[0])
+    b = int(channel_a[1])
+    c = int(channel_a[2])
+    
+    if a not in range(1, 4) or b not in range(1, 8) or c not in range(1, 4):
+        print "请检查 -c 参数是否准确输入。"
+        sys.exit()
+
+    if algorithm_a == "hw":
+        alg = '华为'
+        out = (c-1)*7*3 + (b-1)*3 + a
+    elif algorithm_a == "lx":
+        alg = '朗讯'
+        out = (a-1)*7*3 + (b-1)*3 + c
+    elif algorithm_a == "itu":
+        alg = 'ITU'
+        out = (a-1)*7*3 + b + (c-1)*7
+    else:
+        print "请检查 -a 参数是否准确输入。"
+        sys.exit()
+
+    print "E1 通道 %s 对应的 %s 编号为：%d " %(channel_a, alg, out) 
+
 
 if __name__ == '__main__':
     try:
@@ -51,23 +79,13 @@ if __name__ == '__main__':
                 channel_a = value
     except getopt.GetoptError:
         usage()
+
+    if algorithm_a == "" or channel_a == "000":
+        print "请准确输入 -a/-c 参数！"
+        sys.exit()
+    else:
+        calc(channel_a, algorithm_a)
     
-    aug =  ""
-    a = int(channel_a[0])
-    b = int(channel_a[1])
-    c = int(channel_a[2])
-
-    if algorithm_a == "hw":
-        alg = u'华为'
-        out = (c-1)*7*3 + (b-1)*3 + a
-    if algorithm_a == "lx":
-        alg = u'朗讯'
-        out = (a-1)*7*3 + (b-1)*3 + c
-    if algorithm_a == "itu":
-        alg = 'ITU'
-        out = (a-1)*7*3 + b + (c-1)*7
-
-    print "E1 通道 %s 对应的 %s 编号为：%d " %(channel_a, aug, out) 
 
 
 
